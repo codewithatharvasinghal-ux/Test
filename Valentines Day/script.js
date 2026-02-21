499312931bf82551842c0b61506b06c4bae1b7b5
Atharva Singhal <codewithatharvasinghal@gmail.com>
2026-02-21 15:45:37 +0100

    first commit

---
+++ Valentines Day/script.js
@@ -0,0 +1,9 @@
+let openBtn = document.getElementById("openBtn");
+
+let message = document.getElementById("msg");
+
+openBtn.addEventListener("click", function() {
+    message.classList.add("show");
+});
+
+
